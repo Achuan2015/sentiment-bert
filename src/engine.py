7ca944a52cfb85546d68321b8db392d89ce45b57
zhangchuan <zhangchuan2017@gmail.com>
2021-01-20 00:40:11 +0800

    update

--- src/engine.py
+++ src/engine.py
@@ -1,5 +1,11 @@
 from tqdm import tqdm
+import torch.nn as nn
+import torch
 
+
+def loss_fn(outputs, targets):
+    # TODO attention target format size
+    return nn.BCEWithLogitsLoss()(outputs, targets.view(-1, 1))
 
 def train_fn(data_loader, model, optimizer, device, accumulatiion_steps):
     model.train()
